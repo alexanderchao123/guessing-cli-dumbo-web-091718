08438c4e5e2421dc3327c61a0e3816bc3a4bda3a
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-13 20:47:55 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -10,6 +10,8 @@ def run_guessing_game
     else
       puts "The computer guessed #{random_num}."
     end
+    puts "Guess a number between 1 and 6."
+    input = gets().chomp()
   end
   puts "Goodbye!"
 end
