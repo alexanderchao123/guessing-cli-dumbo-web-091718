0d151a6b5f4cea9944b2af75085f11eeb879adf8
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-13 20:32:01 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,6 +2,6 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   input = gets().chomp()
   random_num = Random.rand(1..6)
-  binding.pry
+  require 'pry'; binding.pry
   puts "Goodbye!" if input == "exit"
 end
